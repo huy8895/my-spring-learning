983897093a522444e413565dc7bb5c302170e7eb
huypc <huytv4>
2021-04-26 22:23:51 +0700

    Getting Started Guides 1- Building a RESTful Web Service

---
+++ getting-started-guides/1-rest-service/src/main/java/com/huy8895/restservice/RestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.huy8895.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestServiceApplication.class, args);
+    }
+
+}
