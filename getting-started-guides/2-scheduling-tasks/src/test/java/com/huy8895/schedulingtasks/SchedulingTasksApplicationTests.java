deb8d7e8e018c04fd6fabc986cd4e3a7f5924a50
huypc <huytv4>
2021-04-26 22:40:48 +0700

    Getting Started Guides 2 - Scheduling Tasks

---
+++ getting-started-guides/2-scheduling-tasks/src/test/java/com/huy8895/schedulingtasks/SchedulingTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huy8895.schedulingtasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingTasksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
