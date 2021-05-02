b42be69e2eb7acd58620a4f5f8e86ec05f233260
huypc <huy8895@gmail.com>
2021-05-03 01:11:30 +0700

    upload file s3 bucket

---
+++ viblo-demo/src/main/java/com/example/uploadbucket/BucketController.java
@@ -0,0 +1,21 @@
+package com.example.uploadbucket;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/storage/")
+public class BucketController {
+    private AmazonClient amazonClient;
+
+    @Autowired
+    BucketController(AmazonClient amazonClient) {
+        this.amazonClient = amazonClient;
+    }
+
+    @PostMapping("/uploadFile")
+    public String uploadFile(@RequestPart(value = "file") MultipartFile file) {
+        return amazonClient.uploadFile(file);
+    }
+}
---
+++ viblo-demo/src/test/java/com/example/uploadbucket/UploadBucketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uploadbucket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadBucketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
